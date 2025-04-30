69959fef27b031448c450fc780de0c710cd395b8
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-01 06:06:01 +0800

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import './sober.js';
+// import './sober.js';
+import 'sober';
 import './i18n';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
