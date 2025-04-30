d22913f9880db3e67da1fb6278e52416847ba788
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-01 06:11:51 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';
+// import './index.css';
 // import './sober.js';
 import 'sober';
 import './i18n';
 
-ReactDOM.createRoot(document.body).render(
-    <React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
         <BrowserRouter>
             <App />
         </BrowserRouter>
