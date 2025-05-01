7410ff097ddd93a87c7c95af721bf0756c17a6f7
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-01 08:20:00 +0800

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import 'sober';
-import { Snackbar } from 'sober'
+import { Snackbar } from 'sober';
 import './i18n';
 
-ReactDOM.createRoot(document.body).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
         <BrowserRouter>
             <App />
