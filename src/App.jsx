2db5a5e76a9bf880f6706c37c353a74e2bbfe94b
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-01 11:11:58 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { TitleProvider, useAppTitle } from './TitleContext.jsx';
 export default function App() {
   return (
     <TitleProvider>
-      <Layout />
+      <AppLayout />
     </TitleProvider>
   );
 }
