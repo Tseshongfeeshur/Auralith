cf5171be1c6c68e24a4afedd4eb7e4fd894165d7
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-03 11:39:48 +0800

    update Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,7 +1,7 @@
 import { useRef, useEffect, useState } from 'react';
 import { useAppTitle } from '../TitleContext';
 import { useTranslation } from 'react-i18next';
-import i18n from './i18n';
+import i18n from '../i18n';
 
 // 语言选择器
 function LanguagePicker() {
