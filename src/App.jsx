0236b21c9d653a0fee5a068929894099d820c320
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-03 09:08:01 +0800

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useAppTitle } from './TitleContext.jsx';
+import { NavLink } from "react-router"
 import Library from './pages/Library';
 import Editor from './pages/Editor';
 import About from './pages/About';
@@ -30,7 +31,8 @@ export default function App() {
         <div slot="start">
           <s-menu>
             <div slot="label">{t('navigation')}</div>
-            <s-menu-item onClick={() => navigate('/')} checked="true">
+            <NavLink to="/">
+            <s-menu-item checked="true">
               <s-icon slot="start">
                 <svg viewBox="0 -960 960 960">
                   <path d="M400-400h160v-80H400v80Zm0-120h320v-80H400v80Zm0-120h320v-80H400v80Zm-80 400q-33 0-56.5-23.5T240-320v-480q0-33 23.5-56.5T320-880h480q33 0 56.5 23.5T880-800v480q0 33-23.5 56.5T800-240H320Zm0-80h480v-480H320v480ZM160-80q-33 0-56.5-23.5T80-160v-560h80v560h560v80H160Zm160-720v480-480Z"></path>
@@ -38,6 +40,7 @@ export default function App() {
               </s-icon>
               {t('pages.library')}
             </s-menu-item>
+            </NavLink>
             <s-menu-item onClick={() => navigate('/editor')}>
               <s-icon slot="start">
                 <svg viewBox="0 -960 960 960">
