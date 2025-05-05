24248a4ae117c2e2dd5d8cd0fe9df63c1f686c0c
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-05 16:34:33 +0800

    update Settings.jsx

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -63,7 +63,7 @@ export default function Editor() {
   }, []);
   
   return (
-    <div className={style.container}>
+    <div className={styles.container}>
       <ModeSwitch />
       <s-divider className={styles.divider}></s-divider>
       <LanguagePicker />
