e1211f057ac966b780f21c7987ed1849ac1e319c
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-06 12:22:12 +0800

    update Library.jsx

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -7,22 +7,22 @@ import styles from './styles/Library.module.css';
 
 function Card({ title, id, desc }) {
   return (
-    <s-card>
+    <s-card type="outlined" clickable="true">
       <div>
         <s-icon>
           <svg viewBox="0 -960 960 960">
             <path d="M300-80q-58 0-99-41t-41-99v-520q0-58 41-99t99-41h500v600q-25 0-42.5 17.5T740-220q0 25 17.5 42.5T800-160v80H300Zm-60-267q14-7 29-10t31-3h20v-440h-20q-25 0-42.5 17.5T240-740v393Zm160-13h320v-440H400v440Zm-160 13v-453 453Zm60 187h373q-6-14-9.5-28.5T660-220q0-16 3-31t10-29H300q-26 0-43 17.5T240-220q0 26 17 43t43 17Z"></path>
           </svg>
         </s-icon>
-        <span>
+        <div slot="headline">
           {title}
-        </span>
+        </div>
       </div>
       <div>
-        <span>
+        <div slot="text">
           {desc}
-        </span>
-        <s-icon-button>
+        </div>
+        <s-icon-button slot="action">
           <s-icon>
             <svg viewBox="0 -960 960 960">
               <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"></path>
