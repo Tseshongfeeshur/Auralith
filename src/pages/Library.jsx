93f0fda145ba9048eb46fe5ce2b920599c3caf4d
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-09 23:18:29 +0800

    update Library.jsx

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -194,13 +194,6 @@ export default function Library() {
         </s-icon-button>
       </s-search>
       <GameCards />
-      <s-fab className={styles.fabRefresh} onClick={showAddDialog}>
-        <s-icon>
-          <svg viewBox="0 -960 960 960">
-            <path d="M480-160q-134 0-227-93t-93-227q0-134 93-227t227-93q69 0 132 28.5T720-690v-110h80v280H520v-80h168q-32-56-87.5-88T480-720q-100 0-170 70t-70 170q0 100 70 170t170 70q77 0 139-44t87-116h84q-28 106-114 173t-196 67Z"></path>
-          </svg>
-        </s-icon>
-      </s-fab>
       <s-fab className={styles.fabAdd} onClick={showAddDialog}>
         <s-icon name="add" slot="start"></s-icon>
         {t('library.add-game')}
