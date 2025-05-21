3b9c4f54ea66d9e53835372bf42b38bef060973a
Tseshongfeeshur <ryanyuanzhehan@gmail.com>
2025-05-21 21:59:27 +0800

    update TitleContext.jsx

--- src/lib/TitleContext.jsx
+++ src/lib/TitleContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 const TitleContext = createContext();
